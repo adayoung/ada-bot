7f670e329d27f2a07881c8298d33b3974d87a59f
Ada Young <adayoung@users.noreply.github.com>
2017-04-10 11:13:10 +0000

    We have a basic Discord bot!

---
+++ discord/discord.go
@@ -0,0 +1,94 @@
+package discord
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var BotID string
+var dg *discordgo.Session
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func InitDiscordSession(token string) error {
+	// Create a new Discord session using the provided login information.
+	var err error
+	if dg, err = discordgo.New(fmt.Sprintf("Bot %s", token)); err == nil {
+		if u, err := dg.User("@me"); err == nil {
+			BotID = u.ID
+
+			dg.AddHandler(ready)
+			// Add handlers for messages received
+			dg.AddHandler(messageCreate)
+			if err := dg.Open(); err == nil {
+				fmt.Println("Successfully launched a new Discord session.")
+			} else {
+				return err // Error at opening Discord Session
+			}
+		} else {
+			return err // Error at obtaining account details
+		}
+	} else {
+		return err // Error at creating a new Discord session
+	}
+
+	return nil
+}
+
+func CloseDiscordSession() {
+	dg.Close()
+}
+
+func ready(s *discordgo.Session, r *discordgo.Ready) {
+	if guilds, err := s.UserGuilds(); err != nil {
+		fmt.Println("ERROR: We couldn't get UserGuilds")
+		log.Fatalf("error: %v", err)
+	} else {
+		for index, guild := range guilds {
+			fmt.Printf("[%d] ------------------------------\n", index)
+			fmt.Println("Guild ID: ", guild.ID)
+			fmt.Println("Guild Name: ", guild.Name)
+			fmt.Println("Guild Permissions: ", guild.Permissions)
+
+		}
+		fmt.Println("----------------------------------")
+	}
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotID { // ignore the bot's own messages from processing
+		return
+	}
+
+	if c, err := s.State.Channel(m.ChannelID); err != nil {
+		fmt.Println("Oops, error at getting session.State.Channel,", err)
+		return // Not a fatal error
+	} else {
+		if c.GuildID == "" {
+			fmt.Printf("Message received from %s: %s\n", m.Author.Username, m.Content)
+			if strings.ToLower(m.Content) == "ping" {
+				_, _ = s.ChannelMessageSend(m.ChannelID, "Pong!")
+			}
+		}
+	}
+
+	if strings.ToLower(m.Content) == "!ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	if strings.ToLower(m.Content) == "!pink" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "I love pink!")
+	}
+
+	if strings.HasPrefix(strings.ToLower(m.Content), "!decide") {
+		choices := strings.Split(m.Content[8:], " or ")
+		the_answer := choices[rand.Intn(len(choices))]
+		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("The correct answer is **%s**", the_answer))
+	}
+}
--- main.go
+++ main.go
@@ -1,29 +1,68 @@
 package main
 
 import (
-		"fmt"
-		"os"
-		"os/signal"
-		"syscall"
-		"time"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	// "time"
+
+	"gopkg.in/yaml.v2"
+
+	"ada-bot/discord"
 )
 
+type config struct {
+	Discord struct {
+		BotKey  string
+		Channel string
+	}
+
+	IronRealms struct {
+		GameFeed string
+	}
+}
+
+var _config config
+
+func init() {
+	if data, err := ioutil.ReadFile("env.yaml"); err == nil {
+		if err := yaml.Unmarshal([]byte(data), &_config); err != nil {
+			fmt.Println("ERROR: Error with parsing env.yaml.")
+			log.Fatalf("error: %v", err)
+		}
+	} else {
+		fmt.Println("ERROR: The file 'env.yaml' could not be read.")
+		log.Fatalf("error: %v", err)
+	}
+}
+
 func main() {
-		c := make(chan os.Signal, 2)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		go func() {
-				<-c
-				fmt.Println("cleanup")
-				os.Exit(0)
-		}()
+	err := discord.InitDiscordSession(_config.Discord.BotKey)
+	if err != nil {
+		fmt.Println("ERROR: We couldn't initialize a Discord session.")
+		log.Fatalf("error: %v", err)
+	}
 
-		ticker := time.NewTicker(time.Millisecond * 3000) // 3 second ticker
-		go func() {
-				for t := range ticker.C {
-					fmt.Println("Tick at", t)
-				}
-		}()
+	// Setup signals handling here
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Interrupt received, cleaning up...")
+		discord.CloseDiscordSession()
+		os.Exit(0)
+	}()
 
-		fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-		<-make(chan int) // block forever till SIGINT / SIGTERM
+	// ticker := time.NewTicker(time.Millisecond * 3000) // 3 second ticker
+	// go func() {
+	// 	for t := range ticker.C {
+	// 		fmt.Println("Tick at", t)
+	// 	}
+	// }()
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	<-make(chan int) // block forever till SIGINT / SIGTERM
 }
